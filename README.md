64b829b47ac7f7886878f8d23bea881efb5ab4d1
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-02-09 02:56:39 +0700

    D-Bus signal handler, Sliding mode

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
+# polybar-spotify: add D-Bus signal handler, sliding text mode
+
+### Dependencies
+- Python (2.x or 3.x)
+- Python [`dbus`](https://pypi.org/project/dbus-python/) module
+- Python `gi.repository` module
+- Python [`time`](https://docs.python.org/3/library/time.html) module
+- playerctl
+
+### Configuration
+```ini
+[module/spotify]
+type = custom/script
+exec = python -u /path/to/spotify/script.py -i 668 -s
+;important
+tail = true
+
+;choose any icon you line or not use any icon 
+format-prefix = "  "
+format-prefix-foreground = #1db954
+
+;line under module, you should set your-bar's line-size
+format-underline = #1db954
+
+;control players (optional)
+click-left = playerctl --player=spotify play-pause
+click-right = playerctl --player=spotify next
+click-middle = playerctl --player=spotify previous
+```
+
+#### Custom arguments
+
+##### Truncate, Status indicator, Fonts, Quiet
+As original...
+
+##### Format
+As original, but remove the {play_pause} option. There is always a play/pause button at the beginning
+of output (if you want to remove this button, issue in this repository).
+
+##### Slide
+The option "-s" or "--slide" is used to decide the behavior of this program when the display text's length exceed trunclen. If set, display content in sliding mode. Otherwise the behavior is similar to the original.
+
+You should use mono font if this flag is set.
+
+##### Interval
+The option "-i" or "--interval" is used to decide number of **miliseconds** between each label update in sliding mode (it affect the sliding speed).
+
+##### Append string
+In sliding mode, append a string after displayed content.
+
+====================================================
+
+**Original README**
+
 # polybar-spotify
 
 This is a module that shows the current song playing and its primary artist on Spotify, with a Spotify-green underline, for people that don't want to set up mpd. If Spotify is not active, nothing is shown. If the song name is longer than `trunclen` characers (default 25), it is truncated and `...` is appended. If the song is truncated and contains a single opening parenthesis, the closing paranethsis is appended as well.
--- spotify_status.py
+++ spotify_status.py
@@ -1,40 +1,55 @@
 #!/usr/bin/env python
 
-import sys
-import dbus
 import argparse
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+import sys
+import time
 
+def fix_string(string):
+    # corrects encoding for the python version used
+    if sys.version_info.major == 3:
+        return string
+    else:
+        return string.encode('utf-8')
+
+# python parsing library
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '-t',
     '--trunclen',
     type=int,
-    metavar='trunclen'
+    metavar='<maximum number of characters displayed>',
+    default=35,
+    dest='trunclen'
 )
 parser.add_argument(
     '-f',
     '--format',
     type=str,
-    metavar='custom format',
-    dest='custom_format'
+    metavar='<custom format>',
+    dest='custom_format',
+    default=fix_string(u'{artist}: {song}')
 )
 parser.add_argument(
     '-p',
     '--playpause',
     type=str,
-    metavar='play-pause indicator',
-    dest='play_pause'
+    metavar='<play-pause indicator>',
+    dest='play_pause',
+    default=fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
 )
 parser.add_argument(
     '--font',
     type=str,
-    metavar='the index of the font to use for the main label',
+    metavar="<main label font index>",
     dest='font'
 )
 parser.add_argument(
     '--playpause-font',
     type=str,
-    metavar='the index of the font to use to display the playpause indicator',
+    metavar='<play-pause button font index>',
     dest='play_pause_font'
 )
 parser.add_argument(
@@ -44,48 +59,63 @@ parser.add_argument(
     help="if set, don't show any output when the current song is paused",
     dest='quiet',
 )
-
+parser.add_argument(
+    '-i',
+    '--interval',
+    type=int,
+    metavar='<label updating frequency>',
+    help='number of miliseconds between each update() call',
+    dest='interval',
+    default=1000
+)
+parser.add_argument(
+    '-s',
+    '--slide',
+    action='store_true',
+    help="if set, long label won't be truncated but be displayed in sliding mode",
+    dest='slide',
+)
+parser.add_argument(
+    '-a',
+    '--append',
+    type=str,
+    help="this will be appended to the displayed string for sliding mode",
+    dest='append',
+    default='        ',
+    metavar='<appended string>'
+)
 args = parser.parse_args()
 
-
-def fix_string(string):
-    # corrects encoding for the python version used
-    if sys.version_info.major == 3:
-        return string
-    else:
-        return string.encode('utf-8')
-
-
-def truncate(name, trunclen):
-    if len(name) > trunclen:
-        name = name[:trunclen]
-        name += '...'
-        if ('(' in name) and (')' not in name):
-            name += ')'
-    return name
-
-
-
-# Default parameters
-output = fix_string(u'{play_pause} {artist}: {song}')
-trunclen = 35
-play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
-
-label_with_font = '%{{T{font}}}{label}%{{T-}}'
+# argument parsing
 font = args.font
 play_pause_font = args.play_pause_font
-
+trunclen = args.trunclen
+output_format = args.custom_format
+play_pause = args.play_pause
 quiet = args.quiet
+interval = args.interval
+slide = args.slide
+append = args.append
 
-# parameters can be overwritten by args
-if args.trunclen is not None:
-    trunclen = args.trunclen
-if args.custom_format is not None:
-    output = args.custom_format
-if args.play_pause is not None:
-    play_pause = args.play_pause
+# formating template
+label_with_font = '%{{T{font}}}{label}%{{T-}}'
+label_with_button = '{button} {label}'
+
+# Handle play/pause label
+play_pause = play_pause.split(',')
+if play_pause_font is not None:
+    play_pause[0] = label_with_font.format(font=play_pause_font, label=play_pause[0])
+    play_pause[1] = label_with_font.format(font=play_pause_font, label=play_pause[1])
 
 try:
+    ##########################################
+    ### DBus properties changed signal handler
+    ##########################################
+
+    # Setting for asynchronous method calls
+    DBusGMainLoop(set_as_default=True)
+
+    # Connect to the bus
     session_bus = dbus.SessionBus()
     spotify_bus = session_bus.get_object(
         'org.mpris.MediaPlayer2.spotify',
@@ -96,43 +126,106 @@ try:
         spotify_bus,
         'org.freedesktop.DBus.Properties'
     )
+    # Spotify metadata from d-bus
+    artist = None
+    song = None
+    album = None
+    play_pause_status = None
 
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    # Variable for sliding window label
+    displayStringStartPos = 0
 
-    # Handle play/pause label
+    def getProperties():
+        global spotify_properties, artist, song, album, play_pause_status
+        metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+        play_pause_status = fix_string( spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus') )
+        artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
+        song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+        album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
 
-    play_pause = play_pause.split(',')
+    #signal updateProperties(interface_name, changed_properties, invalidated_properties):
+    def updateProperties(*args, **kwargs):
+        (iface, changed_props, invalidated_prop) = args
+        assert (iface == "org.mpris.MediaPlayer2.Player")
+        global artist, song, album, play_pause_status, displayStringStartPos
+        for property, value in changed_props.items():
+            match property:
+                case "PlaybackStatus":
+                    play_pause_status = fix_string(value)
+                case "Metadata":
+                    artist = fix_string(value['xesam:artist'][0]) if value['xesam:artist'] else ''
+                    song = fix_string(value['xesam:title']) if value['xesam:title'] else ''
+                    album = fix_string(value['xesam:album']) if value['xesam:album'] else ''
+                    displayStringStartPos = 0
+        updateLabel(callFromGLib=False)
 
-    if status == 'Playing':
-        play_pause = play_pause[0]
-    elif status == 'Paused':
-        play_pause = play_pause[1]
-    else:
-        play_pause = str()
+    ##########################################
+    ### Label updater
+    ##########################################
+    def updateLabel(*args, **kwargs):
+        # argument reader
+        func_args = {
+            "callFromGLib": True
+        }
+        for key, value in kwargs.items():
+            assert (key in func_args)
+            func_args[key] = value
 
-    if play_pause_font:
-        play_pause = label_with_font.format(font=play_pause_font, label=play_pause)
+        # final argument
+        callFromGLib = func_args["callFromGLib"]
 
-    # Handle main label
+        if (quiet and status == 'Paused') or (not artist and not song and not album):
+            print('')
+        else:
+            label = output_format.format(artist=artist, 
+                                         song=song,
+                                         album=album)
+            label_len = len(label)
+            display = label
+            if trunclen < label_len:
+                if slide:
+                    label += append
+                    label_len += len(append)
 
-    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
-    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
-    album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
+                    global displayStringStartPos
+                    display = label[displayStringStartPos:displayStringStartPos + trunclen]
+                    if displayStringStartPos + trunclen > label_len:
+                        display += label[:displayStringStartPos + trunclen - label_len]
 
-    if (quiet and status == 'Paused') or (not artist and not song and not album):
-        print('')
-    else:
-        if font:
-            artist = label_with_font.format(font=font, label=artist)
-            song = label_with_font.format(font=font, label=song)
-            album = label_with_font.format(font=font, label=album)
+                    if callFromGLib:
+                        displayStringStartPos += 1
+                        if displayStringStartPos == label_len:
+                            displayStringStartPos = 0
 
-        # Add 4 to trunclen to account for status symbol, spaces, and other padding characters
-        print(truncate(output.format(artist=artist, 
-                                     song=song, 
-                                     play_pause=play_pause, 
-                                     album=album), trunclen + 4))
+                else:
+                    display = label[:trunclen] + '...'
+
+            if font is not None:
+                display = label_with_font.format(label=display,
+                                                 font=font)
+            print(label_with_button.format(button=play_pause[0] if play_pause_status=="Playing"
+                                                                else play_pause[1],
+                                           label=display))
+
+        # never stop update label
+        return True
+
+    ##########################################
+    ### Program main loop
+    ##########################################
+
+    # Declare two loops
+    spotify_properties.connect_to_signal("PropertiesChanged", updateProperties)
+    if slide:
+        GLib.timeout_add(interval, updateLabel)
+
+    # First output (no initial delay)
+    getProperties()
+    updateLabel()
+
+    # Loop forever
+    loop = GLib.MainLoop()
+    loop.run()
 
 except Exception as e:
     if isinstance(e, dbus.exceptions.DBusException):
